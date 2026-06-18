12447ae5c7ac2f8d5482f4cc7ca182deb6fa2ecf
maheesha <mdewmini99@gmail.com>
2026-06-18 11:36:17 +0530

    fix: remove helmet, use manual CORS headers to resolve 403 error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -12,8 +11,16 @@ const notesRoutes = require('./routes/notesRoutes');
 
 const app = express();
 
-app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(morgan('dev'));
 app.use(express.json());
 
